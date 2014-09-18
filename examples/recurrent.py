9ef76659ca4fe08fda83ce1a0d664b32e0eb0697
Colin Raffel <craffel@gmail.com>
2014-09-18 13:31:37 -0400

    First implementation of a recurrent layer, with example.

---
+++ examples/recurrent.py
@@ -0,0 +1,68 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import nntools
+
+
+def gen_data(length):
+    '''
+    Generate a simple lag sequence
+
+    :parameters:
+        - length : int
+            Length of sequences to generate
+
+    :returns:
+        - X : np.ndarray, shape=(length, 2)
+            Input sequence
+        - y : np.ndarray, shape=(length, 1)
+            Target sequence, where y[n] = X[n - 1] - *X[n - 2] + noise
+    '''
+    X = np.random.rand(length, 2)
+    y = np.zeros((length, 1))
+    # Compute y[n] = X[n - 1] - *X[n - 2] + noise
+    delayed_X = X[1:-1, 0] - X[:-2, 1] + .01*np.random.randn(length - 2)
+    # Store as target
+    y[2:, :] = delayed_X.reshape(-1, 1)
+    return X, y
+
+# Sequence length
+length = 10
+# Number of units in the hidden (recurrent) layer
+n_hidden = 4
+
+# Generate a "validation" sequence whose cost we will periodically compute
+X_val, y_val = gen_data(length)
+
+# Construct vanilla RNN: One recurrent layer (with input weights) and one
+# dense output layer
+l_in = nntools.layers.InputLayer(shape=(1, X_val.shape[1]))
+nonlinearity = nntools.nonlinearities.tanh
+l_recurrent = nntools.layers.RecurrentLayer(l_in,
+                                            num_units=n_hidden,
+                                            nonlinearity=nonlinearity)
+l_out = nntools.layers.DenseLayer(l_recurrent, num_units=y_val.shape[1],
+                                  nonlinearity=nonlinearity)
+
+# Cost function is mean squared error
+input = T.matrix('input')
+target_output = T.matrix('target_output')
+cost = T.mean((l_out.get_output(input)[2:] - target_output[2:])**2)
+# Use SGD for training
+learning_rate = .1
+all_params = nntools.layers.get_all_params(l_out)
+updates = nntools.updates.sgd(cost, all_params, learning_rate)
+# Theano functions for training, getting output, and computing cost
+train = theano.function([input, target_output], cost, updates=updates)
+y_pred = theano.function([input], l_out.get_output(input))
+compute_cost = theano.function([input, target_output], cost)
+
+# Train the net
+n_iterations = 10000
+costs = np.zeros(n_iterations)
+for n in range(n_iterations):
+    X, y = gen_data(length)
+    costs[n] = train(X, y)
+    if not n % 1000:
+        cost_val = compute_cost(X_val, y_val)
+        print "Iteration {} validation cost = {}".format(n, cost_val)
--- nntools/layers/base.py
+++ nntools/layers/base.py
@@ -238,6 +238,51 @@ class GaussianNoiseLayer(Layer):
             return input + _srng.normal(input.shape, avg=0.0, std=self.sigma)
 
 
+class RecurrentLayer(Layer):
+    def __init__(self, input_layer, num_units, W_input=init.Normal(0.01),
+                 W_recurrent=init.Normal(0.01), b=init.Constant(0.),
+                 h=init.Constant(0.),
+                 nonlinearity=nonlinearities.rectify):
+        super(RecurrentLayer, self).__init__(input_layer)
+        if nonlinearity is None:
+            self.nonlinearity = nonlinearities.identity
+        else:
+            self.nonlinearity = nonlinearity
+
+        self.num_units = num_units
+
+        num_inputs = self.input_layer.get_output_shape()[1]
+
+        self.W_input = self.create_param(W_input, (num_inputs, num_units))
+        self.W_recurrent = self.create_param(W_recurrent,
+                                             (num_units, num_units))
+        self.b = self.create_param(b, (num_units,))
+        self.h = self.create_param(h, (num_units,))
+
+    def get_params(self):
+        return [self.W_input, self.W_recurrent, self.b]
+
+    def get_bias_params(self):
+        return [self.b]
+
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0], self.num_units)
+
+    def get_output_for(self, input, *args, **kwargs):
+        if input.ndim > 2:
+            input = input.reshape((input.shape[0], T.prod(input.shape[1:])))
+
+        # Create single recurrent computation step function
+        def step(layer_input, previous_output, W_input, W_recurrent, b):
+            return self.nonlinearity(T.dot(layer_input, W_input) +
+                                     T.dot(previous_output, W_recurrent) +
+                                     b)
+        return theano.scan(step, sequences=input, outputs_info=[self.h],
+                           non_sequences=[self.W_input,
+                                          self.W_recurrent,
+                                          self.b])[0]
+
+
 ## Convolutions
 
 class Conv1DLayer(Layer):

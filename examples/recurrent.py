c5a239e9339ca7bf6fa9e0b739fc1fba68ce92bd
Colin Raffel <craffel@gmail.com>
2014-11-06 13:00:31 -0500

    More cleanup/comments

--- examples/recurrent.py
+++ examples/recurrent.py
@@ -11,6 +11,10 @@ N_HIDDEN = 4
 N_BATCH = 30
 # Delay used to generate artificial training data
 DELAY = 2
+# SGD learning rate
+LEARNING_RATE = 1e-5
+# Number of iterations to train the net
+N_ITERATIONS = 1000
 
 
 def gen_data(length=LENGTH, n_batch=N_BATCH, delay=DELAY):
@@ -47,10 +51,13 @@ X_val, y_val = gen_data()
 # dense output layer
 l_in = nntools.layers.InputLayer(shape=(N_BATCH, LENGTH, X_val.shape[-1]))
 
+# This input layer is used to tell the input-to-hidden what shape to expect
+# As we iterate over time steps, the input will be batch size x feature dim
 l_recurrent_in = nntools.layers.InputLayer(shape=(N_BATCH, X_val.shape[-1]))
 l_input_to_hidden = nntools.layers.DenseLayer(l_recurrent_in, N_HIDDEN,
                                               nonlinearity=None)
 
+# As above, we need to tell the hidden-to-hidden layer what shape to expect
 l_recurrent_hid = nntools.layers.InputLayer(shape=(N_BATCH, N_HIDDEN))
 l_hidden_to_hidden = nntools.layers.DenseLayer(l_recurrent_hid, N_HIDDEN,
                                                nonlinearity=None,
@@ -75,19 +82,17 @@ target_output = T.tensor3('target_output')
 # Cost = mean squared error, starting from delay point
 cost = T.mean((l_out.get_output(input)[:, DELAY:, :]
                - target_output[:, DELAY:, :])**2)
-# Use SGD for training
-learning_rate = 1e-5
+# Use NAG for training
 all_params = nntools.layers.get_all_params(l_out)
-updates = nntools.updates.nesterov_momentum(cost, all_params, learning_rate)
+updates = nntools.updates.nesterov_momentum(cost, all_params, LEARNING_RATE)
 # Theano functions for training, getting output, and computing cost
 train = theano.function([input, target_output], cost, updates=updates)
 y_pred = theano.function([input], l_out.get_output(input))
 compute_cost = theano.function([input, target_output], cost)
 
 # Train the net
-n_iterations = 1000
-costs = np.zeros(n_iterations)
-for n in range(n_iterations):
+costs = np.zeros(N_ITERATIONS)
+for n in range(N_ITERATIONS):
     X, y = gen_data()
     costs[n] = train(X, y)
     if not n % 100:

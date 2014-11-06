4404a612ae5bc4bdcf460b5bff6cbfa7b0d4fd63
Colin Raffel <craffel@gmail.com>
2014-11-06 12:08:05 -0500

    Allowing batches of time series to be provided

--- examples/recurrent.py
+++ examples/recurrent.py
@@ -4,56 +4,73 @@ import theano.tensor as T
 import nntools
 
 
-def gen_data(length):
+def gen_data(length, n_batch):
     '''
     Generate a simple lag sequence
 
     :parameters:
         - length : int
             Length of sequences to generate
+        - n_batch : int
+            Number of training sequences per batch
 
     :returns:
-        - X : np.ndarray, shape=(length, 2)
+        - X : np.ndarray, shape=(n_batch, length, 2)
             Input sequence
-        - y : np.ndarray, shape=(length, 1)
+        - y : np.ndarray, shape=(n_batch, length, 1)
             Target sequence, where y[n] = X[n - 1] - *X[n - 2] + noise
     '''
-    X = np.random.rand(length, 2)
-    y = np.zeros((length, 1))
+    X = np.random.rand(n_batch, length, 2)
+    y = np.zeros((n_batch, length, 1))
     # Compute y[n] = X[n - 1] - *X[n - 2] + noise
-    delayed_X = X[1:-1, 0] - X[:-2, 1] + .01*np.random.randn(length - 2)
+    delayed_X = (X[:, 1:-1, 0] - X[:, :-2, 1] +
+                 .01*np.random.randn(n_batch, length - 2))
     # Store as target
-    y[2:, :] = delayed_X.reshape(-1, 1)
+    y[:, 2:, :] = delayed_X.reshape(n_batch, length - 2, 1)
     return X, y
 
 # Sequence length
 length = 10
 # Number of units in the hidden (recurrent) layer
 n_hidden = 4
+# Number of training sequences in each batch
+n_batch = 30
 
 # Generate a "validation" sequence whose cost we will periodically compute
-X_val, y_val = gen_data(length)
+X_val, y_val = gen_data(length, n_batch)
 
 # Construct vanilla RNN: One recurrent layer (with input weights) and one
 # dense output layer
-l_in = nntools.layers.InputLayer(shape=(1, X_val.shape[1]))
-l_input_to_hidden = nntools.layers.DenseLayer(l_in, n_hidden,
+l_in = nntools.layers.InputLayer(shape=(n_batch, length, X_val.shape[-1]))
+
+l_recurrent_in = nntools.layers.InputLayer(shape=(n_batch, X_val.shape[-1]))
+l_input_to_hidden = nntools.layers.DenseLayer(l_recurrent_in, n_hidden,
                                               nonlinearity=None)
-l_hidden_to_hidden = nntools.layers.DenseLayer(l_input_to_hidden, n_hidden,
+
+l_recurrent_hid = nntools.layers.InputLayer(shape=(n_batch, n_hidden))
+l_hidden_to_hidden = nntools.layers.DenseLayer(l_recurrent_hid, n_hidden,
                                                nonlinearity=None,
                                                b=nntools.init.Constant(1.))
-l_recurrent = nntools.layers.RecurrentLayer(l_in, l_input_to_hidden,
+
+l_recurrent = nntools.layers.RecurrentLayer(l_in,
+                                            l_input_to_hidden,
                                             l_hidden_to_hidden,
                                             nonlinearity=None)
-l_out = nntools.layers.DenseLayer(l_recurrent, num_units=y_val.shape[1],
-                                  nonlinearity=None)
+l_reshape = nntools.layers.ReshapeLayer(l_recurrent,
+                                        (n_batch*length, n_hidden))
+
+l_recurrent_out = nntools.layers.DenseLayer(l_reshape,
+                                            num_units=y_val.shape[-1],
+                                            nonlinearity=None)
+l_out = nntools.layers.ReshapeLayer(l_recurrent_out,
+                                    (n_batch, length, y_val.shape[-1]))
 
 # Cost function is mean squared error
-input = T.matrix('input')
-target_output = T.matrix('target_output')
-cost = T.mean((l_out.get_output(input)[2:] - target_output[2:])**2)
+input = T.tensor3('input')
+target_output = T.tensor3('target_output')
+cost = T.mean((l_out.get_output(input)[:, 2:, :] - target_output[:, 2:, :])**2)
 # Use SGD for training
-learning_rate = .1
+learning_rate = 1e-4
 all_params = nntools.layers.get_all_params(l_out)
 updates = nntools.updates.nesterov_momentum(cost, all_params, learning_rate)
 # Theano functions for training, getting output, and computing cost
@@ -65,7 +82,7 @@ compute_cost = theano.function([input, target_output], cost)
 n_iterations = 1000
 costs = np.zeros(n_iterations)
 for n in range(n_iterations):
-    X, y = gen_data(length)
+    X, y = gen_data(length, n_batch)
     costs[n] = train(X, y)
     if not n % 100:
         cost_val = compute_cost(X_val, y_val)

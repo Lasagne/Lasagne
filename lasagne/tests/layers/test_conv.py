b6474818f615330f3bac318c1d03436393922552
Daniel Nouri <daniel.nouri@gmail.com>
2015-01-25 23:00:19 +0100

    Use conv2d to calculate expected outputs, set flip_filters; add Conv2DCCLayer test.

--- lasagne/tests/layers/test_conv.py
+++ lasagne/tests/layers/test_conv.py
@@ -1,80 +1,25 @@
 from mock import Mock
 import numpy as np
 import pytest
+import theano
+from theano.tensor.nnet import conv2d
 
 from lasagne.utils import floatX
 
 
-def input_kernel_and_output_one_sample():
-    input = np.array([[[
-        [1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 0],
-        [0, 0, 1, 1, 1],
-        [0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0],
-        ]]])
-    kernel = np.array([[[
-        [1, 0, 1],
-        [0, 1, 0],
-        [1, 0, 1],
-        ]]])
-    output = np.array([[[
-        [4, 3, 4],
-        [2, 4, 3],
-        [2, 3, 4],
-        ]]])
-    return map(floatX, [input, kernel, output])
+def conv2d_test_sets():
+    def _convert(input, kernel, output, kwargs):
+        return [theano.shared(floatX(input)), floatX(kernel), output, kwargs]
 
+    input = np.random.random((3, 1, 16, 16))
+    kernel = np.random.random((16, 1, 3, 3))
+    output = conv2d(input, kernel).eval()
+    yield _convert(input, kernel, output, {})
 
-def input_kernel_and_output_multiple_samples():
-    input, kernel, output = input_kernel_and_output_one_sample()
-
-    input = np.repeat(input, 5, axis=0)
-    output = np.repeat(output, 5, axis=0)
-
-    return map(floatX, [input, kernel, output])
-
-
-def input_kernel_and_output_multiple_filters():
-    input, kernel, output = input_kernel_and_output_one_sample()
-
-    kernel = np.array([
-        [[
-            [1, 0, 1],
-            [0, 1, 0],
-            [1, 0, 1],
-        ]],
-        [[
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 0, 0],
-        ]],
-    ])
-
-    output = np.array([[
-        [
-            [4, 3, 4],
-            [2, 4, 3],
-            [2, 3, 4],
-        ],
-        [
-            [2, 3, 2],
-            [1, 2, 3],
-            [1, 2, 2],
-        ],
-        ]])
-
-    return map(floatX, [input, kernel, output])
-
-
-def input_kernel_and_output_multiple_channels():
-    input, kernel, output = input_kernel_and_output_multiple_filters()
-
-    # all the inputs channels are identical:
-    input = np.repeat(input, 2, axis=1)
-    kernel = kernel.transpose(1, 0, 2, 3)
-    output = output.sum(axis=1)[None, :]
-    return map(floatX, [input, kernel, output])
+    input = np.random.random((3, 3, 16, 16))
+    kernel = np.random.random((16, 3, 3, 3))
+    output = conv2d(input, kernel).eval()
+    yield _convert(input, kernel, output, {})
 
 
 @pytest.fixture
@@ -90,31 +35,38 @@ def DummyInputLayer():
 class TestConv2DLayerImplementations:
     @pytest.fixture(
         params=[
-            "Conv2DLayer",
-            "Conv2DMMLayer",
-            "Conv2DDNNLayer",  # untested
+            ('Conv2DLayer', {}),
+            ('Conv2DCCLayer', {'flip_filters': True}),
+            ('Conv2DMMLayer', {'flip_filters': True}),
+            ('Conv2DDNNLayer', {'flip_filters': True}),
             ],
         )
     def Conv2DImpl(self, request):
         from lasagne import layers
+        impl_name, impl_default_kwargs = request.param
         try:
-            return getattr(layers, request.param)
+            impl = getattr(layers, impl_name)
         except AttributeError:
             pytest.skip("{} not available".format(request.param))
 
-    @pytest.mark.parametrize("input, kernel, output", [
-        input_kernel_and_output_one_sample(),
-        input_kernel_and_output_multiple_samples(),
-        input_kernel_and_output_multiple_filters(),
-        input_kernel_and_output_multiple_channels(),
-        ])
-    @pytest.mark.parametrize("kwargs", [
+        def wrapper(*args, **kwargs):
+            kwargs2 = impl_default_kwargs.copy()
+            kwargs2.update(kwargs)
+            return impl(*args, **kwargs2)
+
+        wrapper.__name__ = impl_name
+        return wrapper
+
+    @pytest.mark.parametrize(
+        "input, kernel, output, kwargs", list(conv2d_test_sets()))
+    @pytest.mark.parametrize("extra_kwargs", [
         {},
         {'untie_biases': True},
         ])
     def test_defaults(self, Conv2DImpl, DummyInputLayer,
-                      input, kernel, output, kwargs):
-        input_layer = DummyInputLayer(input.shape)
+                      input, kernel, output, kwargs, extra_kwargs):
+        kwargs.update(extra_kwargs)
+        input_layer = DummyInputLayer(input.shape.eval())
         layer = Conv2DImpl(
             input_layer,
             num_filters=kernel.shape[0],
@@ -124,4 +76,4 @@ class TestConv2DLayerImplementations:
             )
         actual = layer.get_output(input).eval()
         assert actual.shape == output.shape
-        assert (actual == output).all()
+        assert np.allclose(actual, output)

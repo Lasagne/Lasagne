c32c1b2f870800f2b744917dc50f4a9136f5bf7d
Geoffrey French <frondit1985@googlemail.com>
2015-01-31 21:13:20 +0000

    Objectives now have an optional mask.

--- lasagne/objectives.py
+++ lasagne/objectives.py
@@ -1,35 +1,50 @@
+import theano
 import theano.tensor as T
 
 
-def mse(x, t):
+def mse(x, t, m):
     """Calculates the MSE mean across all dimensions, i.e. feature
      dimension AND minibatch dimension.
 
     :parameters:
         - x : predicted values
         - t : target values
+        - m : mask; None for no mask, or a 1-dimensional array, that
+            selects which rows in `x` and `t` should count towards the
+            result
 
     :returns:
         - output : the mean square error across all dimensions
     """
-    return T.mean((x - t) ** 2)
+    if m is None:
+        return T.mean((x - t) ** 2)
+    else:
+        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
+        return T.sum(m * (x - t) ** 2) * recip_mask_sum
 
 
-def crossentropy(x, t):
+def crossentropy(x, t, m):
     """Calculates the binary crossentropy mean across all dimentions,
     i.e.  feature dimension AND minibatch dimension.
 
     :parameters:
         - x : predicted values
         - t : target values
+        - m : mask; None for no mask, or a 1-dimensional array, that
+            selects which rows in `x` and `t` should count towards the
+            result
 
     :returns:
         - output : the mean binary cross entropy across all dimensions
     """
-    return T.mean(T.nnet.binary_crossentropy(x, t))
+    if m is None:
+        return T.mean(T.nnet.binary_crossentropy(x, t))
+    else:
+        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
+        return T.sum(T.nnet.binary_crossentropy(x, t)) * recip_mask_sum
 
 
-def multinomial_nll(x, t):
+def multinomial_nll(x, t, m):
     """Calculates the mean multinomial negative-log-loss
 
     :parameters:
@@ -41,11 +56,21 @@ def multinomial_nll(x, t):
             a 1D integer array that gives the class index of each sample
             (the position of the 1 in the row in a 1-of-N encoding, or
             1-hot encoding),
+        - m : mask; None for no mask, or a 1-dimensional array, that
+            selects which rows in `x` and `t` should count towards the
+            result
 
     :returns:
         - output : the mean multinomial negative log loss
     """
-    return T.mean(T.nnet.categorical_crossentropy(x, t))
+    if m is None:
+        return -T.mean(T.log(x)[T.arange(t.shape[0]), t])
+    else:
+        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
+        masked_nll_sum = T.sum(T.log(x)[T.arange(t.shape[0]), t] * m)
+        return -masked_nll_sum * recip_mask_sum
+
+
 
 
 class Objective(object):
@@ -69,8 +94,9 @@ class Objective(object):
         self.input_layer = input_layer
         self.loss_function = loss_function
         self.target_var = T.matrix("target")
+        self.mask_var = None
 
-    def get_loss(self, input=None, target=None, *args, **kwargs):
+    def get_loss(self, input=None, target=None, mask=None, *args, **kwargs):
         """
         Get loss scalar expression
 
@@ -80,6 +106,10 @@ class Objective(object):
             - target : (default `None`) an expression that results in the
                 desired output that the network is being trained to generate
                 given the input
+            - mask : (default `None`) [optional] an expression that results in
+                a mask. Zero values in the mask will prevent the values in
+                the corresponding rows in `input` and `target` from counting
+                toward the loss value that is returned
             - args : additional arguments passed to `input_layer`'s
                 `get_output` method
             - kwargs : additional keyword arguments passed to `input_layer`'s
@@ -91,5 +121,7 @@ class Objective(object):
         network_output = self.input_layer.get_output(input, *args, **kwargs)
         if target is None:
             target = self.target_var
+        if mask is None:
+            mask = self.mask_var
 
-        return self.loss_function(network_output, target)
+        return self.loss_function(network_output, target, mask)

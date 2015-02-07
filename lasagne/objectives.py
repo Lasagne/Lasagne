ea9d6b33394771a973e3788b974719239e22d8e0
Geoffrey French <frondit1985@gmail.com>
2015-02-07 12:20:14 +0000

    Removed mask normalization from loss functions. Implemented new MaskedObjective class that incorporates mask normalization.

--- lasagne/objectives.py
+++ lasagne/objectives.py
@@ -2,16 +2,15 @@ import theano
 import theano.tensor as T
 
 
-def mse(x, t, m):
+def mse(x, t, m=None):
     """Calculates the MSE mean across all dimensions, i.e. feature
      dimension AND minibatch dimension.
 
     :parameters:
         - x : predicted values
         - t : target values
-        - m : mask; None for no mask, or a 1-dimensional array, that
-            selects which rows in `x` and `t` should count towards the
-            result
+        - m : mask; None for no mask, or an array the same shape as `t`
+            that selects/weights the contribution of elements in `x` and `t`
 
     :returns:
         - output : the mean square error across all dimensions
@@ -19,20 +18,18 @@ def mse(x, t, m):
     if m is None:
         return T.mean((x - t) ** 2)
     else:
-        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
-        return T.sum(m * (x - t) ** 2) * recip_mask_sum
+        return T.sum(((x - t) ** 2) * m)
 
 
-def crossentropy(x, t, m):
+def crossentropy(x, t, m=None):
     """Calculates the binary crossentropy mean across all dimentions,
     i.e.  feature dimension AND minibatch dimension.
 
     :parameters:
         - x : predicted values
         - t : target values
-        - m : mask; None for no mask, or a 1-dimensional array, that
-            selects which rows in `x` and `t` should count towards the
-            result
+        - m : mask; None for no mask, or an array the same shape as `t`
+            that selects/weights the contribution of elements in `x` and `t`
 
     :returns:
         - output : the mean binary cross entropy across all dimensions
@@ -40,35 +37,34 @@ def crossentropy(x, t, m):
     if m is None:
         return T.mean(T.nnet.binary_crossentropy(x, t))
     else:
-        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
-        return T.sum(T.nnet.binary_crossentropy(x, t)) * recip_mask_sum
+        return T.sum(T.nnet.binary_crossentropy(x, t) * m)
 
 
-def multinomial_nll(x, t, m):
+def multinomial_nll(x, t, m=None):
     """Calculates the mean multinomial negative-log-loss
 
     :parameters:
         - x : (predicted) class probabilities; a theano expression resulting
-            in a 2D array; samples run along axis 0, class probabilities along
-            axis 1
+            in a 2D array; samples run along axis 0, class probabilities
+            along axis 1
         - t : (correct) class probabilities; a theano expression resulting in
             a 2D tensor that gives the class probabilities in its rows, OR
             a 1D integer array that gives the class index of each sample
             (the position of the 1 in the row in a 1-of-N encoding, or
             1-hot encoding),
-        - m : mask; None for no mask, or a 1-dimensional array, that
-            selects which rows in `x` and `t` should count towards the
-            result
+        - m : mask; None for no mask, or a 1D array that selects/weights
+            the contributions of the log-loss scores of each sample before
+            they are summed
 
     :returns:
         - output : the mean multinomial negative log loss
     """
     if m is None:
-        return -T.mean(T.log(x)[T.arange(t.shape[0]), t])
+        return T.mean(T.nnet.categorical_crossentropy(x, t))
     else:
-        recip_mask_sum = T.cast(1.0 / T.sum(m), theano.config.floatX)
-        masked_nll_sum = T.sum(T.log(x)[T.arange(t.shape[0]), t] * m)
-        return -masked_nll_sum * recip_mask_sum
+        if m.ndim != 1:
+            raise ValueError, 'mask must be a 1D array'
+        return T.sum(T.nnet.categorical_crossentropy(x, t) * m)
 
 
 
@@ -77,8 +73,8 @@ class Objective(object):
     """
     Training objective
 
-    The  `get_loss` method returns an expression that can be used for training
-    with a gradient descent approach.
+    The  `get_loss` method returns an expression that can be used for
+    training with a gradient descent approach.
     """
     def __init__(self, input_layer, loss_function=mse):
         """
@@ -94,22 +90,79 @@ class Objective(object):
         self.input_layer = input_layer
         self.loss_function = loss_function
         self.target_var = T.matrix("target")
-        self.mask_var = None
 
-    def get_loss(self, input=None, target=None, mask=None, *args, **kwargs):
+
+    def get_loss(self, input=None, target=None, *args, **kwargs):
         """
         Get loss scalar expression
 
         :parameters:
-            - input : (default `None`) an expression that results in the input
-                data that is passed to the network
+            - input : (default `None`) an expression that results in the
+                input data that is passed to the network
             - target : (default `None`) an expression that results in the
                 desired output that the network is being trained to generate
                 given the input
-            - mask : (default `None`) [optional] an expression that results in
-                a mask. Zero values in the mask will prevent the values in
-                the corresponding rows in `input` and `target` from counting
-                toward the loss value that is returned
+            - args : additional arguments passed to `input_layer`'s
+                `get_output` method
+            - kwargs : additional keyword arguments passed to `input_layer`'s
+                `get_output` method
+
+        :returns:
+            - output : loss expressions
+        """
+        network_output = self.input_layer.get_output(input, *args, **kwargs)
+        if target is None:
+            target = self.target_var
+
+        return self.loss_function(network_output, target)
+
+
+
+
+
+class MaskedObjective(object):
+    """
+    Masked training objective
+
+    The  `get_loss` method returns an expression that can be used for
+    training with a gradient descent approach, with masking applied to weight
+    the contribution of samples to the final loss.
+    """
+    def __init__(self, input_layer, loss_function=mse, normalize_mask=False):
+        """
+        Constructor
+
+        :parameters:
+            - input_layer : a `Layer` whose output is the networks prediction
+                given its input
+            - loss_function : a loss function of the form `f(x, t, m)` that
+                returns a scalar loss given tensors that represent the
+                predicted values, true values and mask as arguments.
+        """
+        self.input_layer = input_layer
+        self.loss_function = loss_function
+        self.target_var = T.matrix("target")
+        self.mask_var = T.matrix("mask")
+        self.normalize_mask = normalize_mask
+
+    def get_loss(self, input=None, target=None, mask=None,
+                 normalize_mask=None, *args, **kwargs):
+        """
+        Get loss scalar expression
+
+        :parameters:
+            - input : (default `None`) an expression that results in the
+                input data that is passed to the network
+            - target : (default `None`) an expression that results in the
+                desired output that the network is being trained to generate
+                given the input
+            - mask : None for no mask, or a mask that is the same shape
+                as `target`/`self.target_var` - or will broadcast to that
+                shape - that selects/weights the contributions of
+                the samples to the final loss
+            - normalize_mask : None to use the value passed to the
+                constructor, or a bool to override it. If True, the mask will
+                be normalized by dividing it by its sum before being applied.
             - args : additional arguments passed to `input_layer`'s
                 `get_output` method
             - kwargs : additional keyword arguments passed to `input_layer`'s
@@ -123,5 +176,11 @@ class Objective(object):
             target = self.target_var
         if mask is None:
             mask = self.mask_var
+
+        if normalize_mask is None:
+            normalize_mask = self.normalize_mask
+
+        if normalize_mask:
+            mask = mask / T.cast(1.0 / T.sum(mask), theano.config.floatX)
 
         return self.loss_function(network_output, target, mask)
--- lasagne/tests/test_objectives.py
+++ lasagne/tests/test_objectives.py
@@ -6,26 +6,71 @@ def test_mse():
     from lasagne.objectives import mse
 
     output = np.array([
-        [1.0, 0.0, 1.0, 0.0],
+        [1.0, 0.0, 3.0, 0.0],
         [-1.0, 0.0, -1.0, 0.0],
         ])
     target = np.zeros((2, 4))
+    mask = np.array([[1.0], [0.0]])
 
+    # Sqr-error sum = 1**2 + (-1)**2 + (-1)**2 + 3**2 = 12
+    # Mean is 1.5
     result = mse(output, target)
-    assert result.eval() == 0.5
+    assert result.eval() == 1.5
+    # Masked error sum is 1**2 + 3**2
+    result_with_mask = mse(output, target, mask)
+    assert result_with_mask.eval() == 10
 
 
 def test_crossentropy():
     from lasagne.objectives import crossentropy
 
     output = np.array([
-        [np.e ** -2],
-        [np.e ** -1],
+        [np.e ** -2]*4,
+        [np.e ** -1]*4,
         ])
     target = np.ones((2, 4))
+    mask = np.array([[0.0], [0.25]])
 
+    # Cross entropy sum is (2*4) + (1*4) = 12
+    # Mean is 1.5
     result = crossentropy(output, target)
     assert result.eval() == 1.5
+    # Masked cross entropy sum is 1*4*0.25 = 1
+    result_with_mask = crossentropy(output, target, mask)
+    assert result_with_mask.eval() == 1
+
+
+def test_multinomial_nll():
+    from lasagne.objectives import multinomial_nll
+
+    output = np.array([
+        [1.0, 1.0-np.e**-1, np.e**-1],
+        [1.0-np.e**-2, np.e**-2, 1.0],
+        [1.0-np.e**-3, 1.0, np.e**-3]
+        ])
+    target_1hot = np.array([2,1,2])
+    target_2d = np.array([
+        [0.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+    ])
+    mask_1hot = np.array([0,1,1])
+
+    # Multinomial NLL sum is 1 + 2 + 3 = 6
+    # Mean is 2
+    result = multinomial_nll(output, target_1hot)
+    assert result.eval() == 2
+    # Multinomial NLL sum is (0*0 + 1*0 + 1*1) + (2*0 + 2*1 + 0*0)
+    # + (3*0 + 0*0 + 3*1) = 6
+    # Mean is 2
+    result = multinomial_nll(output, target_2d)
+    assert result.eval() == 2
+    # Masked NLL sum is 2 + 3 = 5
+    result_with_mask = multinomial_nll(output, target_1hot, mask_1hot)
+    assert result_with_mask.eval() == 5
+    # Masked NLL sum is 2 + 3 = 5
+    result_with_mask = multinomial_nll(output, target_2d, mask_1hot)
+    assert result_with_mask.eval() == 5
 
 
 def test_objective():
@@ -62,4 +107,25 @@ def test_objective_no_target():
     network_output = input_layer.get_output.return_value
 
     loss_function.assert_called_with(network_output, objective.target_var)
+    assert result == loss_function.return_value
+
+
+def test_masked_objective():
+    from lasagne.objectives import MaskedObjective
+
+    input_layer = mock.Mock()
+    loss_function = mock.Mock()
+    input, target, mask, arg1, kwarg1 = (object(),) * 5
+    objective = MaskedObjective(input_layer, loss_function)
+    result = objective.get_loss(input, target, mask, False, arg1, kwarg1=kwarg1)
+
+    # We expect that the input layer's `get_output` was called with
+    # the `input` argument we provided, plus the extra positional and
+    # keyword arguments.
+    input_layer.get_output.assert_called_with(input, arg1, kwarg1=kwarg1)
+    network_output = input_layer.get_output.return_value
+
+    # The `network_output` and `target` are fed into the loss
+    # function:
+    loss_function.assert_called_with(network_output, target, mask)
     assert result == loss_function.return_value

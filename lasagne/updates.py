d9e8b10e5e51965c024810eddbd66ee42813278f
Eric Battenberg <ebattenberg@gracenote.com>
2015-02-10 14:22:01 -0800

    add initial implementation of norm_constraint

--- lasagne/updates.py
+++ lasagne/updates.py
@@ -11,7 +11,7 @@ import theano.tensor as T
 def sgd(loss, all_params, learning_rate):
     all_grads = theano.grad(loss, all_params)
     updates = []
-    
+
     for param_i, grad_i in zip(all_params, all_grads):
         updates.append((param_i, param_i - learning_rate * grad_i))
 
@@ -21,7 +21,7 @@ def sgd(loss, all_params, learning_rate):
 def momentum(loss, all_params, learning_rate, momentum=0.9):
     all_grads = theano.grad(loss, all_params)
     updates = []
-    
+
     for param_i, grad_i in zip(all_params, all_grads):
         mparam_i = theano.shared(np.zeros(param_i.get_value().shape, dtype=theano.config.floatX))
         v = momentum * mparam_i - learning_rate * grad_i
@@ -36,7 +36,7 @@ def momentum(loss, all_params, learning_rate, momentum=0.9):
 def nesterov_momentum(loss, all_params, learning_rate, momentum=0.9):
     all_grads = theano.grad(loss, all_params)
     updates = []
-    
+
     for param_i, grad_i in zip(all_params, all_grads):
         mparam_i = theano.shared(np.zeros(param_i.get_value().shape, dtype=theano.config.floatX))
         v = momentum * mparam_i - learning_rate * grad_i # new momemtum
@@ -49,7 +49,7 @@ def nesterov_momentum(loss, all_params, learning_rate, momentum=0.9):
 
 def adagrad(loss, all_params, learning_rate=1.0, epsilon=1e-6):
     """
-    epsilon is not included in the typical formula, 
+    epsilon is not included in the typical formula,
     See "Notes on AdaGrad" by Chris Dyer for more info.
     """
     all_grads = theano.grad(loss, all_params)
@@ -112,4 +112,76 @@ def adadelta(loss, all_params, learning_rate=1.0, rho=0.95, epsilon=1e-6):
         updates.append((acc_delta_i, acc_delta_i_new))
 
     return updates
+
+
+def norm_constraint(orig_update, param=None, abs_max=None, rel_max=None):
+    '''
+    Max weight norm constraints
+
+    This takes a parameter update and rescales it so that incoming weight
+    norms are below a specified constraint value.  The constraint value is
+    the lesser of `abs_max` (if provided) and `rel_max` (if provided) times
+    the average norm of the values originally stored in `param`.
+
+    Right now this supports:
+        * 2D param matrices with shape (input_dim, output_dim)
+        * 4D param tensors with shape (output_chans, input_chans, dim0, dim1)
+    '''
+
+    constraint = None
+
+    if rel_max is not None:
+
+        if param is None:
+            raise ValueError("`rel_max` requires that `param` is provided")
+
+        # Compute average norm of `param`
+        vals = param.get_value()
+        if vals.ndim == 4: # Conv2DLayer weights [chan_out, chan_in, dim0, dim1]
+            sum_over = (1, 2, 3)
+        elif vals.ndim == 2: # DenseLayer weights [in_dim, out_dim]
+            sum_over = (0,)
+        else:
+            raise ValueError(
+                "Unsupported param dimensionality {}".format(vals.ndim)
+            )
+
+        avg_norm = np.mean(np.sqrt(np.sum(vals**2, axis=sum_over)))
+
+        constraint = rel_max * avg_norm
+
+    if abs_max is not None:
+
+        constraint = abs_max if constraint is None else min(abs_max, constraint)
+
+
+    if orig_update.ndim == 4:
+        sum_over = (1, 2, 3)
+        broadcast = (0, 'x', 'x', 'x')
+    elif orig_update.ndim == 2:
+        sum_over = (0,)
+        broadcast = ('x', 0)
+    else:
+        raise ValueError(
+            "Unsupported update dimensionality {}".format(orig_update.ndim)
+        )
+
+    norms = T.sqrt(T.sum(T.sqr(orig_update), axis=sum_over))
+    target_norms = T.clip(norms, 0, constraint)
+    update = orig_update * (target_norms / (1e-7 + norms)).dimshuffle(*broadcast)
+
+    return update
+
+
+
+
+
+
+
+
+
+
+
+
+
 

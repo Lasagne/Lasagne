a7bbef21f732ba6af9b166972de53372f4923a9e
Eric Battenberg <ebattenberg@gmail.com>
2015-02-18 16:22:30 -0800

    add tests for exceptions

--- lasagne/tests/test_updates.py
+++ lasagne/tests/test_updates.py
@@ -1,3 +1,4 @@
+import pytest
 
 PCT_TOLERANCE = 1E-5
 
@@ -26,6 +27,19 @@ def test_compute_norms_axes():
 
     assert array.dtype == norms.dtype
     assert norms.shape == (array.shape[1], array.shape[3])
+
+
+def test_compute_norms_ndim6_raises():
+    import numpy as np
+    import theano
+    from lasagne.updates import compute_norms
+
+    array = np.random.randn(1, 2, 3, 4, 5, 6).astype(theano.config.floatX)
+
+    with pytest.raises(ValueError) as excinfo:
+        norms = compute_norms(array)
+
+    assert "Unsupported tensor dimensionality" in str(excinfo.value)
 
 
 def test_norm_constraint_abs():
@@ -95,3 +109,18 @@ def test_norm_constraint_norm_axes():
     assert param.dtype == update.dtype
     assert (np.max(compute_norms(param.get_value(), norm_axes=norm_axes))
             <= abs_max*(1 + PCT_TOLERANCE))
+
+def test_norm_constraint_abs_dim6_raises():
+    import numpy as np
+    import theano
+    from lasagne.updates import norm_constraint, compute_norms
+
+    abs_max = 0.01
+
+    param = theano.shared(
+        np.random.randn(1, 2, 3, 4, 5, 6).astype(theano.config.floatX)
+    )
+
+    with pytest.raises(ValueError) as excinfo:
+        update = norm_constraint(param, abs_max=abs_max)
+    assert "Unsupported tensor dimensionality" in str(excinfo.value)

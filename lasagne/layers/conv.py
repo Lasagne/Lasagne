fix calculation of conv layers get_output_shape, add tests

lasagne/layers/conv.py
@@ -13,6 +13,24 @@ __all__ = [
 ]
 
 
+def conv_output_length(input_length, filter_length,
+                       stride, border_mode):
+    if border_mode == 'valid':
+        output_length = input_length - filter_length + 1
+    elif border_mode == 'full':
+        output_length = input_length + filter_length - 1
+    elif border_mode == 'same':
+        output_length = input_length
+    else:
+        raise RuntimeError('Invalid border mode: {0}'.format(border_mode))
+
+    # This is the integer arithmetic equivalent to
+    # np.ceil(output_length / stride)
+    output_length = (output_length + stride - 1) // stride
+
+    return output_length
+
+
 class Conv1DLayer(Layer):
     def __init__(self, incoming, num_filters, filter_length, stride=1,
                  border_mode="valid", untie_biases=False, W=init.Uniform(),
@@ -52,16 +70,10 @@ class Conv1DLayer(Layer):
         return [self.b] if self.b is not None else []
 
     def get_output_shape_for(self, input_shape):
-        if self.border_mode == 'valid':
-            output_length = ((input_shape[2] - self.filter_length) //
-                             self.stride + 1)
-        elif self.border_mode == 'full':
-            output_length = ((input_shape[2] + self.filter_length) //
-                             self.stride - 1)
-        elif self.border_mode == 'same':
-            output_length = input_shape[2] // self.stride
-        else:
-            raise RuntimeError("Invalid border mode: '%s'" % self.border_mode)
+        output_length = conv_output_length(input_shape[2],
+                                           self.filter_length,
+                                           self.stride,
+                                           self.border_mode)
 
         return (input_shape[0], self.num_filters, output_length)
 
@@ -138,21 +150,15 @@ class Conv2DLayer(Layer):
         return [self.b] if self.b is not None else []
 
     def get_output_shape_for(self, input_shape):
-        if self.border_mode == 'valid':
-            output_rows = ((input_shape[2] - self.filter_size[0]) //
-                           self.strides[0] + 1)
-            output_columns = ((input_shape[3] - self.filter_size[1]) //
-                              self.strides[1] + 1)
-        elif self.border_mode == 'full':
-            output_rows = ((input_shape[2] + self.filter_size[0] +
-                            self.strides[0] - 2) // self.strides[0])
-            output_columns = ((input_shape[3] + self.filter_size[1] +
-                               self.strides[1] - 2) // self.strides[1])
-        elif self.border_mode == 'same':
-            output_rows = input_shape[2] // self.strides[0]
-            output_columns = input_shape[3] // self.strides[1]
-        else:
-            raise RuntimeError("Invalid border mode: '%s'" % self.border_mode)
+        output_rows = conv_output_length(input_shape[2],
+                                         self.filter_size[0],
+                                         self.strides[0],
+                                         self.border_mode)
+
+        output_columns = conv_output_length(input_shape[3],
+                                            self.filter_size[1],
+                                            self.strides[1],
+                                            self.border_mode)
 
         return (input_shape[0], self.num_filters, output_rows, output_columns)
 

lasagne/tests/layers/test_conv.py
@@ -12,15 +12,39 @@ def conv2d_test_sets():
     def _convert(input, kernel, output, kwargs):
         return [theano.shared(floatX(input)), floatX(kernel), output, kwargs]
 
-    input = np.random.random((3, 1, 16, 16))
-    kernel = np.random.random((16, 1, 3, 3))
-    output = conv2d(input, kernel).eval()
-    yield _convert(input, kernel, output, {})
+    for border_mode in ['valid', 'full', 'same']:
+        conv_mode = 'full' if border_mode == 'same' else border_mode
 
-    input = np.random.random((3, 3, 16, 16))
-    kernel = np.random.random((16, 3, 3, 3))
-    output = conv2d(input, kernel).eval()
-    yield _convert(input, kernel, output, {})
+        for stride in [1, 2, 3]:
+            input = np.random.random((3, 1, 16, 16))
+            kernel = np.random.random((16, 1, 3, 3))
+            output = conv2d(input, kernel,
+                            border_mode=conv_mode,
+                            subsample=(stride, stride),
+                            ).eval()
+            if border_mode == 'same':
+                shift_x = (kernel.shape[2] - 1) // 2
+                shift_y = (kernel.shape[3] - 1) // 2
+                output = output[:, :, shift_x:input.shape[2] + shift_x,
+                                shift_y:input.shape[3] + shift_y]
+            yield _convert(input, kernel, output, {'border_mode': border_mode,
+                                                   'strides': (stride, stride)
+                                                   })
+
+            input = np.random.random((3, 3, 16, 16))
+            kernel = np.random.random((16, 3, 3, 3))
+            output = conv2d(input, kernel,
+                            border_mode=conv_mode,
+                            subsample=(stride, stride),
+                            ).eval()
+            if border_mode == 'same':
+                shift_x = (kernel.shape[2] - 1) // 2
+                shift_y = (kernel.shape[3] - 1) // 2
+                output = output[:, :, shift_x:input.shape[2] + shift_x,
+                                shift_y:input.shape[3] + shift_y]
+            yield _convert(input, kernel, output, {'border_mode': border_mode,
+                                                   'strides': (stride, stride)
+                                                   })
 
 
 @pytest.fixture
@@ -80,4 +104,5 @@ class TestConv2DLayerImplementations:
             )
         actual = layer.get_output(input).eval()
         assert actual.shape == output.shape
+        assert actual.shape == layer.get_output_shape()
         assert np.allclose(actual, output)
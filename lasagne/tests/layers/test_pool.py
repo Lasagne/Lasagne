a74872193d39024d58fc8378cabf638cc59d8a20
Sander Dieleman <sanderdieleman@gmail.com>
2015-03-31 20:48:15 +0200

    test pool results (with ignore_border=True)

--- lasagne/tests/layers/test_pool.py
+++ lasagne/tests/layers/test_pool.py
@@ -4,6 +4,24 @@ import pytest
 import theano
 
 
+def max_pool_1d(data, ds):
+    data_truncated = data[:, :, :(data.shape[2] // ds) * ds]
+    data_pooled = data_truncated.reshape((-1, ds)).max(axis=1)
+    return data_pooled.reshape(data.shape[:2] + (data.shape[2] // ds,))
+
+
+def max_pool_2d(data, ds):
+    data_truncated = data[:, :, :(data.shape[2] // ds[0]) * ds[0],
+                          :(data.shape[3] // ds[1]) * ds[1]]
+    data_reshaped = data_truncated.reshape((-1, data.shape[2] // ds[0], ds[0],
+                                            data.shape[3] // ds[1], ds[1]))
+
+    data_pooled = data_reshaped.max(axis=4).max(axis=2)
+
+    return data_pooled.reshape(data.shape[:2] + (data.shape[2] // ds[0],
+                                                 data.shape[3] // ds[1]))
+
+
 class TestMaxPool1DLayer:
     @pytest.fixture(params=[(32, 64, 128), (None, 64, 128)])
     def input_layer(self, request):
@@ -14,7 +32,19 @@ class TestMaxPool1DLayer:
         from lasagne.layers.pool import MaxPool1DLayer
         return MaxPool1DLayer(input_layer, ds=2)
 
-    def test_get_output_for(self, layer):
+    @pytest.fixture
+    def layer_ignoreborder(self, input_layer):
+        from lasagne.layers.pool import MaxPool1DLayer
+        return MaxPool1DLayer(input_layer, ds=2, ignore_border=True)
+
+    def test_get_output_for(self, layer_ignoreborder):
+        input = numpy.random.randn(32, 64, 128)
+        input_theano = theano.shared(input)
+        result = layer_ignoreborder.get_output_for(input_theano)
+        result_eval = result.eval()
+        assert numpy.all(result_eval == max_pool_1d(input, 2))
+
+    def test_get_output_for_shape(self, layer):
         input = theano.shared(numpy.ones((32, 64, 128)))
         result = layer.get_output_for(input)
         result_eval = result.eval()
@@ -35,7 +65,19 @@ class TestMaxPool2DLayer:
         from lasagne.layers.pool import MaxPool2DLayer
         return MaxPool2DLayer(input_layer, ds=(2, 2))
 
-    def test_get_output_for(self, layer):
+    @pytest.fixture
+    def layer_ignoreborder(self, input_layer):
+        from lasagne.layers.pool import MaxPool2DLayer
+        return MaxPool2DLayer(input_layer, ds=(2, 2), ignore_border=True)
+
+    def test_get_output_for(self, layer_ignoreborder):
+        input = numpy.random.randn(32, 64, 24, 24)
+        input_theano = theano.shared(input)
+        result = layer_ignoreborder.get_output_for(input_theano)
+        result_eval = result.eval()
+        assert numpy.all(result_eval == max_pool_2d(input, (2, 2)))
+
+    def test_get_output_for_shape(self, layer):
         input = theano.shared(numpy.ones((32, 64, 24, 24)))
         result = layer.get_output_for(input)
         result_eval = result.eval()

d8947812da3e96f2771b28365f8a47719b2f5813
Eben Olson <eben.olson@gmail.com>
2015-04-07 22:30:55 -0400

    update test_pool for stride and pad parameters

--- lasagne/tests/layers/test_pool.py
+++ lasagne/tests/layers/test_pool.py
@@ -7,13 +7,14 @@ import theano
 def max_pool_1d(data, ds, stride=None, pad=0):
     stride = ds if stride is None else stride
 
-    data = np.pad(data, pad, mode='constant')
+    pads = [(0, 0), ] * len(data.shape)
+    pads[-1] = (pad, pad)
+    data = np.pad(data, pads, mode='constant', constant_values=(-np.inf,))
 
     data_shifted = np.zeros((ds,) + data.shape)
     data_shifted = data_shifted[..., :data.shape[-1] - ds + 1]
     for i in range(ds):
         data_shifted[i] = data[..., i:i + data.shape[-1] - ds + 1]
-
     data_pooled = data_shifted.max(axis=0)
 
     if stride:
@@ -22,79 +23,104 @@ def max_pool_1d(data, ds, stride=None, pad=0):
     return data_pooled
 
 
-def max_pool_2d(data, ds):
-    data_pooled = max_pool_1d(data, ds[1])
+def max_pool_2d(data, ds, stride, pad):
+    data_pooled = max_pool_1d(data, ds[1], stride[1], pad[1])
 
     data_pooled = np.swapaxes(data_pooled, -1, -2)
-    data_pooled = max_pool_1d(data_pooled, ds[0])
+    data_pooled = max_pool_1d(data_pooled, ds[0], stride[0], pad[0])
     data_pooled = np.swapaxes(data_pooled, -1, -2)
 
     return data_pooled
 
 
+def pool_test_sets():
+    for ds in [2, 3]:
+        for stride in [1, 2, 3, 4]:
+            for pad in range(ds):
+                yield (ds, stride, pad)
+
+
 class TestMaxPool1DLayer:
-    @pytest.fixture(params=[(32, 64, 128), (None, 64, 128)])
-    def input_layer(self, request):
-        return Mock(get_output_shape=lambda: request.param)
 
-    @pytest.fixture
-    def layer(self, input_layer):
+    def input_layer(self, output_shape):
+        return Mock(get_output_shape=lambda: output_shape)
+
+    def layer_ignoreborder(self, input_layer, ds, stride=None, pad=0):
         from lasagne.layers.pool import MaxPool1DLayer
-        return MaxPool1DLayer(input_layer, ds=2)
+        return MaxPool1DLayer(
+            input_layer,
+            ds=ds,
+            stride=stride,
+            pad=pad,
+            ignore_border=True,
+        )
 
-    @pytest.fixture
-    def layer_ignoreborder(self, input_layer):
-        from lasagne.layers.pool import MaxPool1DLayer
-        return MaxPool1DLayer(input_layer, ds=2, ignore_border=True)
-
-    def test_get_output_for(self, layer_ignoreborder):
-        input = np.random.randn(32, 64, 128)
+    @pytest.mark.parametrize(
+        "ds, stride, pad", list(pool_test_sets()))
+    def test_get_output_for(self, ds, stride, pad):
+        input = np.random.randn(8, 16, 23)
+        input_layer = self.input_layer(input.shape)
         input_theano = theano.shared(input)
-        result = layer_ignoreborder.get_output_for(input_theano)
-        result_eval = result.eval()
-        assert np.allclose(result_eval, max_pool_1d(input, 2))
+        layer_output = self.layer_ignoreborder(
+            input_layer, ds, stride, pad).get_output_for(input_theano)
+        layer_result = layer_output.eval()
+        numpy_result = max_pool_1d(input, ds, stride, pad)
+        assert np.all(numpy_result.shape == layer_result.shape)
+        assert np.allclose(numpy_result, layer_result)
 
-    def test_get_output_for_shape(self, layer):
-        input = theano.shared(np.ones((32, 64, 128)))
-        result = layer.get_output_for(input)
-        result_eval = result.eval()
-        assert result_eval.shape == (32, 64, 64)
-
-    def test_get_output_shape_for(self, layer):
+    @pytest.mark.parametrize(
+        "input_shape", [(32, 64, 128), (None, 64, 128), (32, None, 128)])
+    def test_get_output_shape_for(self, input_shape):
+        input_layer = self.input_layer(input_shape)
+        layer = self.layer_ignoreborder(input_layer, ds=2)
         assert layer.get_output_shape_for((None, 64, 128)) == (None, 64, 64)
         assert layer.get_output_shape_for((32, 64, 128)) == (32, 64, 64)
 
 
 class TestMaxPool2DLayer:
-    @pytest.fixture(params=[(32, 64, 24, 24), (None, 64, 24, 24)])
-    def input_layer(self, request):
-        return Mock(get_output_shape=lambda: request.param)
 
-    @pytest.fixture
-    def layer(self, input_layer):
+    def input_layer(self, output_shape):
+        return Mock(get_output_shape=lambda: output_shape)
+
+    def layer_ignoreborder(self, input_layer, ds, stride=None, pad=(0, 0)):
         from lasagne.layers.pool import MaxPool2DLayer
-        return MaxPool2DLayer(input_layer, ds=(2, 2))
+        return MaxPool2DLayer(
+            input_layer,
+            ds=ds,
+            stride=stride,
+            pad=pad,
+            ignore_border=True,
+        )
 
-    @pytest.fixture
-    def layer_ignoreborder(self, input_layer):
-        from lasagne.layers.pool import MaxPool2DLayer
-        return MaxPool2DLayer(input_layer, ds=(2, 2), ignore_border=True)
-
-    def test_get_output_for(self, layer_ignoreborder):
-        input = np.random.randn(32, 64, 24, 24)
+    @pytest.mark.parametrize(
+        "ds, stride, pad", list(pool_test_sets()))
+    def test_get_output_for(self, ds, stride, pad):
+        input = np.random.randn(8, 16, 17, 13)
+        input_layer = self.input_layer(input.shape)
         input_theano = theano.shared(input)
-        result = layer_ignoreborder.get_output_for(input_theano)
-        result_eval = result.eval()
-        assert np.all(result_eval == max_pool_2d(input, (2, 2)))
+        result = self.layer_ignoreborder(
+            input_layer,
+            (ds, ds),
+            (stride, stride),
+            (pad, pad),
+        ).get_output_for(input_theano)
 
-    def test_get_output_for_shape(self, layer):
-        input = theano.shared(np.ones((32, 64, 24, 24)))
-        result = layer.get_output_for(input)
         result_eval = result.eval()
-        assert result_eval.shape == (32, 64, 12, 12)
 
-    def test_get_output_shape_for(self, layer):
-        assert (layer.get_output_shape_for((None, 64, 24, 24)) ==
-                (None, 64, 12, 12))
-        assert (layer.get_output_shape_for((32, 64, 24, 24)) ==
-                (32, 64, 12, 12))
+        numpy_result = max_pool_2d(
+            input, (ds, ds), (stride, stride), (pad, pad))
+
+        assert np.all(numpy_result.shape == result_eval.shape)
+        assert np.allclose(result_eval, numpy_result)
+
+    @pytest.mark.parametrize(
+        "input_shape",
+        [(32, 64, 24, 24), (None, 64, 24, 24), (32, None, 24, 24)],
+    )
+    def test_get_output_shape_for(self, input_shape):
+        input_layer = self.input_layer(input_shape)
+        layer = self.layer_ignoreborder(input_layer, ds=(2, 2))
+        assert layer.get_output_shape_for(
+            (None, 64, 24, 24)) == (None, 64, 12, 12)
+        assert layer.get_output_shape_for(
+            (32, 64, 24, 24)) == (32, 64, 12, 12)

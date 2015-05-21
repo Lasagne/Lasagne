85df8b520b21a9cdfca6e2bb87c22ed1b8cb866e
Greg <greg@greg-desktop.(none)>
2015-05-21 09:05:59 -0400

    Touched up

--- lasagne/layers/prelu.py
+++ lasagne/layers/prelu.py
@@ -1,5 +1,6 @@
 import numpy as np
 import theano.tensor as T
+import theano
 
 from .. import init
 from .. import nonlinearities
@@ -26,7 +27,7 @@ class PReLULayer(Layer):
         The initial "leakiness" of the rectifier which is then learned.
         If argument not provided, initial leakiness will be set to 0.25.
 
-    untie_alphas : int, array or None
+    untie_alphas : int, iterable, or None
         When passed this argument creates a tensor of alphas rather than a
         single theano scalar. The argument dictates which dimension(s)
         the alphas are untied across. 
@@ -35,38 +36,27 @@ class PReLULayer(Layer):
     -------
         >>> from lasagne.layers import InputLayer, DenseLayer, PReLULayer
         >>> l_in = InputLayer((100, 20))
-        >>> l1 = DenseLayer(l_in, num_units=50, nonlinearity=identity)
+        >>> l1 = DenseLayer(l_in, num_units=50, nonlinearity=None)
         >>> l1_prelu = PReLULayer(l1, alpha=0.35)
     """
     def __init__(self, incoming, alpha=0.25, untie_alphas=None, 
                  **kwargs):
         super(PReLULayer, self).__init__(incoming, **kwargs)
-        self.untie_alphas = untie_alphas
-        if untie_alphas == None:
-            self.alpha_shape = ()
-            self.alpha = self.add_param(utils.floatX(alpha), self.alpha_shape, name="alpha")
-        elif isinstance(untie_alphas, int) and untie_alphas >= 0 and untie_alphas < len(self.input_shape):
-            self.alpha_shape = tuple([self.input_shape[untie_alphas]])
-            self.alpha = self.add_param(utils.floatX(np.zeros(self.alpha_shape))+alpha, self.alpha_shape, name="alpha")
-        elif isinstance(untie_alphas, list) and np.min(untie_alphas) >= 0 and np.max(untie_alphas) < len(self.input_shape):
-            self.alpha_shape = tuple([self.input_shape[x] for x in set(untie_alphas)])
-            self.alpha = self.add_param(utils.floatX(np.zeros(self.alpha_shape))+alpha, self.alpha_shape, name="alpha")
+        if untie_alphas is None:
+            alpha_shape = ()
         else:
-            raise Exception('The untie_alphas parameter is badly formed.')
-
-    def get_params(self):
-        return [self.alpha]
+            untie_alphas = [untie_alphas] if isinstance(untie_alphas, int) else sorted(set(untie_alphas))
+            alpha_shape = tuple([self.input_shape[axis] for axis in untie_alphas])
+        alpha = np.zeros(alpha_shape, dtype=theano.config.floatX) + alpha
+        self.alpha = self.add_param(utils.floatX(alpha), alpha_shape, name='alpha', regularizable=False)
+        self.untie_alphas = untie_alphas
 
     def get_output_for(self, input, **kwargs):
-        untie_dim = ['x']*input.ndim
-        if isinstance(self.untie_alphas, int):
-            untie_dim[self.untie_alphas] = 0
-            untie_dim = tuple(untie_dim)
-            return nonlinearities.LeakyRectify(self.alpha.dimshuffle(untie_dim))(input)
-        elif isinstance(self.untie_alphas, list):
-            for i, dim in enumerate(self.untie_alphas):
-                untie_dim[dim] = i
-            untie_dim = tuple(untie_dim)
-            return nonlinearities.LeakyRectify(self.alpha.dimshuffle(untie_dim))(input)
+        if self.untie_alphas is None:
+            alpha = self.alpha
         else:
-            return nonlinearities.LeakyRectify(self.alpha)(input)
+            pattern = ['x'] * input.ndim
+            for idx, dim in enumerate(self.untie_alphas):
+                pattern[dim] = idx
+            alpha = self.alpha.dimshuffle(pattern)
+        return nonlinearities.LeakyRectify(alpha)(input)

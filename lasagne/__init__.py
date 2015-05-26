d3912df1a2d13a201df062176c95c90f6bce6ab7
Daniel Nouri <daniel.nouri@gmail.com>
2015-05-26 21:41:48 +0200

    Add lasagne.__version__ which mirrors the package's version.

--- lasagne/__init__.py
+++ lasagne/__init__.py
@@ -9,3 +9,8 @@ from . import objectives
 from . import regularization
 from . import updates
 from . import utils
+
+
+import pkg_resources
+__version__ = pkg_resources.get_distribution("Lasagne").version
+del pkg_resources

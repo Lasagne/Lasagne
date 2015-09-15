cd5ed7d3671b0007d1a1d1c46081daaa753ed37c
keskarnitish <keskar.nitish@gmail.com>
2015-09-15 15:59:47 -0500

    fixing issues

--- examples/recurrent_text.py
+++ examples/recurrent_text.py
@@ -1,18 +1,18 @@
 '''
 Recurrent network example.  Trains a vanilla RNN to learn and generate
-text from a user-provided input file. This example is based on Andrej
+text from a user-provided input file. This example is partly based on Andrej
 Karpathy's blog (http://karpathy.github.io/2015/05/21/rnn-effectiveness/)
 and more specifically, on his code min-char-rnn.py
 (https://gist.github.com/karpathy/d4dee566867f8291f086).
-The inputs to the RNN are a sequence of characters and the corresponding
+The inputs to the RNN are a (semi-redundant) sequence of characters and the corresponding
 targets are the characters in the text shifted to the right by one. 
-Assuming a sequence length of 5, a training point for a text file as 
+Assuming a sequence length of 5, a training point for a text file
 "The quick brown fox jumps over the lazy dog" would be
 INPUT : 'T','H','E',' ','Q'
-OUTPUT: 'H','E',' ','Q','U'
+OUTPUT: 'U'
 
-The loss function compares (via categorical crossentropy), the prediction
-at each time step with the output/target.
+The loss function compares (via categorical crossentropy) the prediction
+with the output/target.
 
 Also included is a function to generate text using the RNN given the first 
 character.  
@@ -54,34 +54,44 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 lasagne.random.set_rng(np.random.RandomState(1))
 
 # Sequence Length
-SEQ_LENGTH = 25
+SEQ_LENGTH = 20
 
 # Number of units in the hidden (recurrent) layer
-N_HIDDEN = 100
+N_HIDDEN = 512
 
 # Optimization learning rate
-LEARNING_RATE = .5
+LEARNING_RATE = .1
 
 # All gradients above this will be clipped
 GRAD_CLIP = 5
 
 # How often should we check the output?
-PRINT_FREQ = 1000
+PRINT_FREQ = 200
 
 # Number of epochs to train the net
-NUM_EPOCHS = 10000
+NUM_EPOCHS = 100
 
-def gen_data(p):
+# Batch Size
+BATCH_SIZE = 128
+
+
+def gen_data(p, batch_size = BATCH_SIZE):
     '''
     This function produces a training sample from the location 'p' in the text file.
     For instance, assuming SEQ_LENGTH = 25 and p=0, the function would output the first 
     25 characters of the text file as the input and characters 1-26 (0 indexing) as the target.
     '''
-    x = np.zeros((1,SEQ_LENGTH,vocab_size))
-    y = np.zeros((SEQ_LENGTH,vocab_size))
-    for i in range(SEQ_LENGTH):
-        x[0,i,char_to_ix[data[p+i]]] = 1
-        y[i,char_to_ix[data[p+i+1]]] = 1
+    x = np.zeros((batch_size,SEQ_LENGTH,vocab_size))
+    y = np.zeros(batch_size)
+
+    for n in range(batch_size):
+        ptr = n
+        for i in range(SEQ_LENGTH):
+            try:
+                x[n,i,char_to_ix[data[p+ptr+i]]] = 1.
+            except:
+                import ipdb; ipdb.set_trace()
+        y[n] = char_to_ix[data[p+ptr+SEQ_LENGTH]]
     return x, np.array(y,dtype='int32')
 
 def main(num_epochs=NUM_EPOCHS):
@@ -92,38 +102,43 @@ def main(num_epochs=NUM_EPOCHS):
     # (batch size, SEQ_LENGTH, num_features)
     # For simplicity, the batch size has been hardcoded as 1. However, this is easy to change. 
 
-    l_in = lasagne.layers.InputLayer(shape=(1, None, vocab_size))
+    l_in = lasagne.layers.InputLayer(shape=(None, None, vocab_size))
 
     # We now build the recurrent layer which takes l_in as the input layer
     # The weights are initialized to be Normal with mean 0 and std 0.01
     # For simplicity, we assume that the initial value of the hidden weights is not learnt.
     # We clip the gradients at GRAD_CLIP to prevent the problem of exploding gradients. 
 
-    l_forward = lasagne.layers.RecurrentLayer(
-        l_in, N_HIDDEN, grad_clipping=GRAD_CLIP, learn_init=False,
-        W_in_to_hid=lasagne.init.Normal(),
-        W_hid_to_hid=lasagne.init.Normal(),
+    l_forward_1 = lasagne.layers.LSTMLayer(
+        l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
+        nonlinearity=lasagne.nonlinearities.tanh)
+
+    l_forward_2 = lasagne.layers.LSTMLayer(
+        l_forward_1, N_HIDDEN, grad_clipping=GRAD_CLIP,
         nonlinearity=lasagne.nonlinearities.tanh)
 
     # The l_forward layer creates an output of dimension (batch_size, SEQ_LENGTH, N_HIDDEN)
     # Since we hard-code the batch-size to be one, slice the output to obtain the matrix (SEQ_LENGTH, N_HIDDEN)
-    l_forward_slice = lasagne.layers.SliceLayer(l_forward, 0, 0)
+    l_forward_slice = lasagne.layers.SliceLayer(l_forward_2, -1, 1)
+
+    # l_forward_slice now isolates the last value in the sequence (which corresponds to our prediction)
+    #l_forward_slice = lasagne.layers.SliceLayer(l_forward_single_batch, -1, 0)
 
     # The sliced output is then passed through the softmax nonlinearity to create probability distribution
     # for each time step. In other words, the output of this step is (SEQ_LENGTH, N_HIDDEN)
     l_out = lasagne.layers.DenseLayer(l_forward_slice, num_units=vocab_size, W = lasagne.init.Normal(), nonlinearity=lasagne.nonlinearities.softmax)
 
     # Theano tensor for the targets
-    target_values = T.imatrix('target_output')
+    target_values = T.ivector('target_output')
     
     # lasagne.layers.get_output produces a variable for the output of the net
     network_output = lasagne.layers.get_output(l_out)
 
-        
+       
     # In order to generate text from the network, we need the probability distribution of the next character given the current character
     # This is done using the compiled function probs. 
     # It takes the first character as input (in encoded form) and produces a probability distribution for the next. 
-    probs = theano.function([l_in.input_var],network_output[0])
+    probs = theano.function([l_in.input_var],network_output.mean(axis=0))
 
     def try_it_out(seed, N=200):
         '''
@@ -136,19 +151,20 @@ def main(num_epochs=NUM_EPOCHS):
         '''
         sample_ix = []
 
-        x = np.zeros((1,1,vocab_size))
-        x[0,0,char_to_ix[seed]] = 1.0
+        x,_ = gen_data(8456, 1)
+        
         for i in range(200):
             ix = np.random.choice(range(vocab_size), p=probs(x).ravel())
             sample_ix.append(ix)
-            x = np.zeros((1,1,vocab_size))
-            x[0,0,sample_ix[-1]] = 1.0
+            x = np.zeros((1,SEQ_LENGTH,vocab_size))
+            x[0,0:SEQ_LENGTH-1,:] = x[0,1:,:]
+            x[0,SEQ_LENGTH-1,sample_ix[-1]] = 1. 
 
         random_snippet = seed + ''.join(ix_to_char[ix] for ix in sample_ix)    
         print("----\n %s \n----" % random_snippet)
     
     # The loss function is calculated as the sum of the (categorical) cross-entropy between the prediction and target at each time step
-    cost = T.nnet.categorical_crossentropy(network_output,target_values).sum()
+    cost = T.nnet.categorical_crossentropy(network_output,target_values).mean()
 
     # Retrieve all parameters from the network
     all_params = lasagne.layers.get_all_params(l_out)
@@ -160,31 +176,26 @@ def main(num_epochs=NUM_EPOCHS):
     # Theano functions for training and computing cost
     print("Compiling functions ...")
     train = theano.function([l_in.input_var, target_values], cost, updates=updates)
-    compute_cost = theano.function(
-        [l_in.input_var, target_values], cost)
+    compute_cost = theano.function([l_in.input_var, target_values], cost)
     
-
-    # We'll use this "validation set" to periodically check progress
-    X_val, y_val = gen_data(8456)
-
     print("Training ...")
     p = 0
     try:
-        for it in xrange(data_size * num_epochs):
+        for it in xrange(data_size * num_epochs / BATCH_SIZE):
             sample_ix = try_it_out(data[p]) # Generate text using RNN using the p^th character as the start. 
-            cost_val = compute_cost(X_val, y_val)
-            print("Epoch {} validation cost = {}".format(it*1.0/data_size, cost_val))
+            
+            avg_cost = 0;
             for _ in range(PRINT_FREQ):
                 x,y = gen_data(p)
-                p += SEQ_LENGTH
-                train(x, y)
+                #print(p)
+                p += SEQ_LENGTH + BATCH_SIZE - 1 
+                if(p+BATCH_SIZE+SEQ_LENGTH >= data_size):
+                    print('Carriage Return')
+                    p = 0;
+                
 
-                if(p+1+SEQ_LENGTH>=data_size):
-                    # Bring data pointer back to start 
-                    # Also reset the RNN memory
-                    print('Carriage Returned to Start ...')
-                    p = 0
-                    all_params[0].set_value(np.zeros((1,N_HIDDEN)))
+                avg_cost += train(x, y)
+            print("Epoch {} average loss = {}".format(it*1.0/data_size*BATCH_SIZE, avg_cost / PRINT_FREQ))
                     
     except KeyboardInterrupt:
         pass

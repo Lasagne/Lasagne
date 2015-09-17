c7a9dcc6da5a2ee3348ed551784062603c5b84b4
Jan Schlüter <jan.schlueter@ofai.at>
2015-09-17 15:03:12 +0200

    Ensure lasagne is importable for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,14 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import sys
+import sys
 import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))  # make lasagne available for import
 
 # -- General configuration ------------------------------------------------
 

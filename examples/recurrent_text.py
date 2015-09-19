6bf3a9cdaf7eca38ad5f4e81e271aa4109d7177d
keskarnitish <keskar.nitish@gmail.com>
2015-09-19 16:56:18 -0500

    cleaning up comments

--- examples/recurrent_text.py
+++ examples/recurrent_text.py
@@ -1,10 +1,9 @@
 '''
-Recurrent network example.  Trains a vanilla RNN to learn and generate
+Recurrent network example.  Trains a 2 layered LSTM network to learn and generate
 text from a user-provided input file. This example is partly based on Andrej
 Karpathy's blog (http://karpathy.github.io/2015/05/21/rnn-effectiveness/)
-and more specifically, on his code min-char-rnn.py
-(https://gist.github.com/karpathy/d4dee566867f8291f086).
-The inputs to the RNN are a (semi-redundant) sequence of characters and the corresponding
+and a similar example in the Keras package (keras.io).
+The inputs to the network are batches of sequences of characters and the corresponding
 targets are the characters in the text shifted to the right by one. 
 Assuming a sequence length of 5, a training point for a text file
 "The quick brown fox jumps over the lazy dog" would be
@@ -16,6 +15,8 @@ with the output/target.
 
 Also included is a function to generate text using the RNN given the first 
 character.  
+
+About 20 or so epochs are necessary to generate text that "makes sense".
 
 Written by @keskarnitish
 Pre-processing of text uses snippets of Karpathy's code (BSD License)
@@ -33,7 +34,7 @@ import sys
 #This snippet loads the text file and creates dictionaries to 
 #encode characters into a vector-space representation and vice-versa. 
 try:
-    fname = 'input.txt'
+    fname = 'input.txt' #The provided file is the work of Nietzsche downloaded from https://s3.amazonaws.com/text-datasets/nietzsche.txt
     data = open(fname, 'r').read() # should be simple plain text file
     data = data.decode("utf-8-sig").encode("utf-8")
 except IOError as e:
@@ -42,22 +43,18 @@ except IOError as e:
     print("A sample txt file can be downloaded from https://raw.githubusercontent.com/keskarnitish/Lasagne/master/examples/input.txt")
     sys.exit(1)
 
-
-#As a sample file, you may use the complete works of Shakespeare
-#available as a .txt file from http://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt
-
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 char_to_ix = { ch:i for i,ch in enumerate(chars) }
 ix_to_char = { i:ch for i,ch in enumerate(chars) }
 
-#Lasagne Seed
+#Lasagne Seed for Reproducibility
 lasagne.random.set_rng(np.random.RandomState(1))
 
 # Sequence Length
 SEQ_LENGTH = 20
 
-# Number of units in the hidden (recurrent) layer
+# Number of units in the two hidden (LSTM) layers
 N_HIDDEN = 512
 
 # Optimization learning rate
@@ -78,9 +75,10 @@ BATCH_SIZE = 128
 
 def gen_data(p, batch_size = BATCH_SIZE):
     '''
-    This function produces a training sample from the location 'p' in the text file.
-    For instance, assuming SEQ_LENGTH = 25 and p=0, the function would output the first 
-    25 characters of the text file as the input and characters 1-26 (0 indexing) as the target.
+    This function produces a semi-redundant batch of training samples from the location 'p' in the text file.
+    For instance, assuming SEQ_LENGTH = 25 and p=0, the function would create batches of 
+    25 characters of the text file (starting from the 0th character and stepping by 1 for each semi-redundant batch)
+    as the input and the next character as the target.
     '''
     x = np.zeros((batch_size,SEQ_LENGTH,vocab_size))
     y = np.zeros(batch_size)
@@ -98,13 +96,10 @@ def main(num_epochs=NUM_EPOCHS):
     # First, we build the network, starting with an input layer
     # Recurrent layers expect input of shape
     # (batch size, SEQ_LENGTH, num_features)
-    # For simplicity, the batch size has been hardcoded as 1. However, this is easy to change. 
 
     l_in = lasagne.layers.InputLayer(shape=(None, None, vocab_size))
 
-    # We now build the recurrent layer which takes l_in as the input layer
-    # The weights are initialized to be Normal with mean 0 and std 0.01
-    # For simplicity, we assume that the initial value of the hidden weights is not learnt.
+    # We now build the LSTM layer which takes l_in as the input layer
     # We clip the gradients at GRAD_CLIP to prevent the problem of exploding gradients. 
 
     l_forward_1 = lasagne.layers.LSTMLayer(
@@ -116,14 +111,12 @@ def main(num_epochs=NUM_EPOCHS):
         nonlinearity=lasagne.nonlinearities.tanh)
 
     # The l_forward layer creates an output of dimension (batch_size, SEQ_LENGTH, N_HIDDEN)
-    # Since we hard-code the batch-size to be one, slice the output to obtain the matrix (SEQ_LENGTH, N_HIDDEN)
+    # Since we are only interested in the final prediction, we isolate that quantity and feed it to the next layer. 
+    # The output of the sliced layer will then be of size (batch_size, N_HIDDEN)
     l_forward_slice = lasagne.layers.SliceLayer(l_forward_2, -1, 1)
 
-    # l_forward_slice now isolates the last value in the sequence (which corresponds to our prediction)
-    #l_forward_slice = lasagne.layers.SliceLayer(l_forward_single_batch, -1, 0)
-
-    # The sliced output is then passed through the softmax nonlinearity to create probability distribution
-    # for each time step. In other words, the output of this step is (SEQ_LENGTH, N_HIDDEN)
+    # The sliced output is then passed through the softmax nonlinearity to create probability distribution of the prediction
+    # The output of this stage is (batch_size, vocab_size)
     l_out = lasagne.layers.DenseLayer(l_forward_slice, num_units=vocab_size, W = lasagne.init.Normal(), nonlinearity=lasagne.nonlinearities.softmax)
 
     # Theano tensor for the targets
@@ -132,36 +125,7 @@ def main(num_epochs=NUM_EPOCHS):
     # lasagne.layers.get_output produces a variable for the output of the net
     network_output = lasagne.layers.get_output(l_out)
 
-       
-    # In order to generate text from the network, we need the probability distribution of the next character given the current character
-    # This is done using the compiled function probs. 
-    # It takes the first character as input (in encoded form) and produces a probability distribution for the next. 
-    probs = theano.function([l_in.input_var],network_output,allow_input_downcast=True)
-
-    def try_it_out(seed, N=200):
-        '''
-        This function uses the current state of the RNN to generate text. It takes as input the first character, 
-        computes the probability distribution of the next (using the RNN), chooses one at random using the computed probabilities
-        and continues the process for N characters. 
-        Inputs
-        seed (char) : The first letter of the generated text.
-        N (int) : Number of characters of generated text
-        '''
-        sample_ix = []
-        x,_ = gen_data(seed, 1)
-
-        for i in range(200):
-            #ix = np.random.choice(range(vocab_size), p=probs(x).ravel())
-            ix = np.argmax(probs(x).ravel())
-            sample_ix.append(ix)
-            x[:,0:SEQ_LENGTH-1,:] = x[:,1:,:]
-            x[:,SEQ_LENGTH-1,:] = 0
-            x[0,SEQ_LENGTH-1,sample_ix[-1]] = 1. 
-
-        random_snippet = data[seed] + ''.join(ix_to_char[ix] for ix in sample_ix)    
-        print("----\n %s \n----" % random_snippet)
-    
-    # The loss function is calculated as the sum of the (categorical) cross-entropy between the prediction and target at each time step
+    # The loss function is calculated as the mean of the (categorical) cross-entropy between the prediction and target.
     cost = T.nnet.categorical_crossentropy(network_output,target_values).mean()
 
     # Retrieve all parameters from the network
@@ -175,12 +139,46 @@ def main(num_epochs=NUM_EPOCHS):
     print("Compiling functions ...")
     train = theano.function([l_in.input_var, target_values], cost, updates=updates, allow_input_downcast=True)
     compute_cost = theano.function([l_in.input_var, target_values], cost, allow_input_downcast=True)
+
+    # In order to generate text from the network, we need the probability distribution of the next character given
+    # the state of the network and the input (a seed).
+    # In order to produce the probability distribution of the prediction, we compile a function called probs. 
+    
+    probs = theano.function([l_in.input_var],network_output,allow_input_downcast=True)
+
+    # The next function generates text given a seed using the current state of the network. 
+    # It takes as input a location in the provided text file. 
+    # The function then uses the next SEQ_LENGTH characters as seed to start generating text. 
+    # The input "N" is used to set the number of characters of text to predict. 
+
+    def try_it_out(seed, N=200):
+        '''
+        This function uses the current state of the RNN to generate text. It takes as input the first character, 
+        computes the probability distribution of the next (using the RNN), chooses one at random using the computed probabilities
+        and continues the process for N characters. 
+        Inputs
+        seed (char) : The first letter of the generated text.
+        N (int) : Number of characters of generated text
+        '''
+        sample_ix = []
+        x,_ = gen_data(seed, 1)
+
+        for i in range(N):
+            ix = np.argmax(probs(x).ravel())
+            sample_ix.append(ix)
+            x[:,0:SEQ_LENGTH-1,:] = x[:,1:,:]
+            x[:,SEQ_LENGTH-1,:] = 0
+            x[0,SEQ_LENGTH-1,sample_ix[-1]] = 1. 
+
+        random_snippet = data[seed] + ''.join(ix_to_char[ix] for ix in sample_ix)    
+        print("----\n %s \n----" % random_snippet)
+
     
     print("Training ...")
     p = 0
     try:
         for it in xrange(data_size * num_epochs / BATCH_SIZE):
-            sample_ix = try_it_out(p) # Generate text using RNN using the p^th character as the start. 
+            sample_ix = try_it_out(p) # Generate text using the p^th character as the start. 
             
             avg_cost = 0;
             for _ in range(PRINT_FREQ):

c4be8f6874d865b280ae4b68291b1beb806fbe7f
Jan Schlüter <jan.schlueter@ofai.at>
2016-05-11 12:09:02 +0200

    Fix typo in adadelta docstring

--- lasagne/updates.py
+++ lasagne/updates.py
@@ -461,8 +461,8 @@ def rmsprop(loss_or_grads, params, learning_rate=1.0, rho=0.9, epsilon=1e-6):
 def adadelta(loss_or_grads, params, learning_rate=1.0, rho=0.95, epsilon=1e-6):
     """ Adadelta updates
 
-    Scale learning rates by a the ratio of accumulated gradients to accumulated
-    step sizes, see [1]_ and notes for further description.
+    Scale learning rates by the ratio of accumulated gradients to accumulated
+    updates, see [1]_ and notes for further description.
 
     Parameters
     ----------

0754420d08cc3d375674983166327c060eb4fbcb
sentient07 <vxrram95@gmail.com>
2017-09-04 23:54:23 +0530

    Warn about cuDNN nondeterminism in lasagne/random.py

--- lasagne/random.py
+++ lasagne/random.py
@@ -3,6 +3,13 @@ A module with a package-wide random number generator,
 used for weight initialization and seeding noise layers.
 This can be replaced by a :class:`numpy.random.RandomState` instance with a
 particular seed to facilitate reproducibility.
+
+Note: When using cuDNN, the backward passes of convolutional and max-pooling
+layers will introduce additional nondeterminism (for performance reasons).
+For 2D convolutions, you can enforce a deterministic backward pass
+implementation via the Theano flags ``dnn.conv.algo_bwd_filter=deterministic``
+and ``dnn.conv.algo_bwd_data=deterministic``. Alternatively, you can disable
+cuDNN completely with ``dnn.enabled=False``.
 """
 
 import numpy as np
